e996cb635517870cddcb90864613052ee4983466
Alex <alex@dompro.systems>
2026-01-09 16:29:54 +0200

    Add cache-control headers to prevent stats caching

--- netlify/functions/get-stats.js
+++ netlify/functions/get-stats.js
@@ -114,7 +114,12 @@ export default async (req) => {
       roundCounts,
       champion: championPercentages
     }), {
-      headers: { "Content-Type": "application/json" }
+      headers: { 
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Pragma": "no-cache",
+        "Expires": "0"
+      }
     });
 
   } catch (error) {
